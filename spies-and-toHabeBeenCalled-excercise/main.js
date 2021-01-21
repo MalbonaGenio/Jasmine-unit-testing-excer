ad31cc26889b36a86f5e982561ee1f4ed5be6740
MalbonaGenio <pvalverdemolina@gmail.com>
2021-01-21 23:26:10 +0000

    added spies, and, beenCalledwith excercise.
    We are cheking a confirm on the windows api, we set up a spy to check that windows returns the apropiate values and that its also been called with the apropiate data.

---
+++ spies-and-toHabeBeenCalled-excercise/main.js
@@ -0,0 +1,46 @@
+/*  Unit Testing: Spies Challenge
+*
+*   1. Use a spy and test the method getCodeName() fully
+*   2. You will need to use spyOn and toHaveBeenCalledWith()
+*   3. HINT: you will need to chain the spy 
+*    example: spyOn(object, 'key').and.returnValue(value)
+*/
+
+// Test Suite 
+describe(`${User.name} Class`, () => {
+  let model;
+
+  beforeEach(() => {
+    model = new User();
+  });
+
+  describe('get code name', () => {
+    it('is a coding god when confirmed', () => {
+      //arrange
+      //we need to spyOn the window and the value that returns when it confirms
+      spyOn(window, 'confirm').and.returnValue(true)
+      //act
+      const result = model.getCodeName()
+      //assert
+      expect(result).toBe('TESTING GOD!')
+    })
+
+    it('is a scrub when not confirmed', () => {
+      //arrange
+      spyOn(window, 'confirm').and.returnValue(false)
+      //act
+      const result = model.getCodeName()
+      //assert
+      expect(result).toBe(`Scrub skipping tests in his best friend's ride!`)
+    })
+
+    it('ask a user if they are a testing god', () => {
+      //arrange
+      spyOn(window, 'confirm').and.returnValue(true)
+      //act
+      const result = model.getCodeName()
+      //assert
+      expect(window.confirm).toHaveBeenCalledWith('Are you a testing god?')
+    })
+  });
+});
